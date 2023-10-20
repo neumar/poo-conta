d79e772268934ac35b9cd7cce8a02a955b0ecfbf
Neumar <neumar@ufla.br>
2023-10-20 10:53:07 -0300

    inclui menu

--- Aplicacao.java
+++ Aplicacao.java
@@ -1,21 +1,58 @@
+import java.util.Scanner;
+
 public class Aplicacao {
 
     public static void main(String[] argumentos) {
-        System.out.println("Sistema de Contas Bancarias");
-       
-        Conta c1 = new Conta(0, 1000);
-        Conta c2 = new Conta(300);
+        
+        Scanner entrada = new Scanner(System.in, "utf-8");
 
-        System.out.println("Saldo c1: " + c1.getSaldo());
-        System.out.println("Saldo c2: " + c2.getSaldo());
+        int opcao = 0;
 
-        c1.depositar(500);
-        c2.depositar(-50);
+        Conta c = null;
+        
+        while (opcao != 5) {
 
-        System.out.println("Saldo c1: " + c1.getSaldo());
-        System.out.println("Saldo c2: " + c2.getSaldo());
+            opcao = exibirMenu(entrada);
+
+            if (opcao == 1) {
+                c = new Conta();
+            } else if (opcao == 2) {
+                //mostrar saldo
+                System.out.println("Saldo: " + c.getSaldo());
+            } else if (opcao == 3) {
+                //depositar
+                System.out.println("Informe valor deposito: ");
+                double v = Double.parseDouble(entrada.nextLine());
+                if(c.depositar(v)) {
+                    System.out.println("Deposito realiado com sucesso.");
+                } else {
+                    System.out.println("Erro.");
+                };
+            } else if (opcao == 4) {
+                //sacar
+                System.out.println("Informe valor saque: ");
+                double v = Double.parseDouble(entrada.nextLine());
+                if(c.sacar(v)) {
+                    System.out.println("Saque realiado com sucesso.");
+                } else {
+                    System.out.println("Erro.");
+                };
+            }
+        }
+    }
 
 
+    private static int exibirMenu(Scanner entrada) {
 
+        System.out.println("\n ==== \n Menu");
+        System.out.println("1) Criar conta");
+        System.out.println("2) Consultar saldo");
+        System.out.println("3) Depositar");
+        System.out.println("4) Sacar");
+        System.out.println("5) Sair");
+        System.out.println("Digite uma opcao: ");
+        int opcao = Integer.parseInt(entrada.nextLine());
+
+        return opcao;
     }
 }
--- Conta.java
+++ Conta.java
@@ -53,7 +53,4 @@ public class Conta {
         }
         return false;
     }
-
-
-
 }
